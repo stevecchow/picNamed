d98721a18d7a4205c738b3012282ad3ada43215d
stevechow <zhmnda@icloud.com>
2021-06-27 16:07:42 +0800

    提交

--- change.js
+++ change.js
@@ -6,12 +6,12 @@
  *  1、递归遍历文件夹中的 所有文件
  *  2、打印其 内容创建时间 的 毫秒值
  */
-var fs = require('fs');
-var path = require('path');
+var fs = require("fs");
+var path = require("path");
 
 // 文件源 绝对路径
-var oldPath = '/Users/stevechow/Desktop/1/';
-var newPath = '/Users/stevechow/Desktop/pics/';
+var oldPath = "/Users/stevechow/Desktop/1/";
+var newPath = "/Users/stevechow/Desktop/pics/";
 
 // 总数
 allNmu = 0;
@@ -22,28 +22,38 @@ allNmu = 0;
  * @param {String} newName
  */
 var reName = (oldName, newName) => {
-  fs.rename(oldName, newName, err => {
+  fs.rename(oldName, newName, (err) => {
     if (err) {
-      console.log('命名出错');
+      console.log("命名出错");
       return;
     }
-    console.log('命名成功');
+    console.log("命名成功");
   });
 };
 
+/**
+ * 生成文件名
+ * @param {*} name
+ * @returns
+ */
 var getFileSuffix = (name) => {
   return name.substring(name.lastIndexOf(".") + 1, name.length).toUpperCase();
-}
+};
 
-function formatDate (date) {
-  console.log(date);
+/**
+ * 格式化日期
+ * @param {*} date
+ * @returns
+ */
+function formatDate(date) {
+  // console.log(date);
   var y = date.getFullYear();
-  console.log(y);
+  // console.log(y);
   var m = date.getMonth() + 1;
-  m = m < 10 ? ('0' + m) : m;
+  m = m < 10 ? "0" + m : m;
   var d = date.getDate();
-  d = d < 10 ? ('0' + d) : d;
-  return y + '-' + m + '-' + d;
+  d = d < 10 ? "0" + d : d;
+  return y + "-" + m + "-" + d;
 }
 
 /**
@@ -51,72 +61,73 @@ function formatDate (date) {
  * 实现方式：递归
  * @param {String} filePath
  */
-var fileDisplay = filePath => {
+var fileDisplay = (filePath) => {
   fs.readdir(filePath, (err, files) => {
     if (err) {
-      console.log('读取目录出错');
+      console.log("读取目录出错");
       return;
     }
-    files.forEach(filename => {
+    files.forEach((filename) => {
       var filedir = path.join(filePath, filename);
       fs.stat(filedir, (err, stats) => {
         if (err) {
-          console.log('获取文件状态错误');
+          console.log("获取文件状态错误");
         } else {
           var isFile = stats.isFile();
           var isDir = stats.isDirectory();
+          var isBigger = stats.size === 4096;
           // 该判断里面的就是文件
-          if (isFile) {
+          if (isFile && !isBigger) {
             allNmu++;
             var timeName = formatDate(stats.birthtime);
-            console.log('stats >>> ', stats)
-            console.log('birthtime >>> ', formatDate(stats.birthtime))
-
+            console.log("stats >>> ", stats);
+            // console.log("birthtime >>> ", formatDate(stats.birthtime));
+            
             //对图片进行复制并重命名
             var oldName = filedir;
-            var newName = newPath + timeName + '-' + allNmu;
-            var suffix = getFileSuffix(oldName)
+            var newName = newPath + timeName + "-" + allNmu;
+            var suffix = getFileSuffix(oldName);
 
-            console.log('suffix >>> ', suffix)
+            console.log("suffix >>> ", suffix);
 
             // 对不同的文件进行分类
             switch (suffix) {
-              case 'JPG':
-                newName = newName + '.JPG';
+              case "JPG":
+                newName = newName + ".JPG";
                 break;
 
-              case 'JPEG':
-                newName = newName + '.JPEG';
+              case "JPEG":
+                newName = newName + ".JPEG";
                 break;
 
-              case 'PNG':
-                newName = newName + '.PNG';
+              case "PNG":
+                newName = newName + ".PNG";
                 break;
 
-              case 'GIF':
-                newName = newName + '.GIF';
+              case "GIF":
+                newName = newName + ".GIF";
                 break;
 
-              case 'HEIC':
-                newName = newName + '.HEIC';
+              case "HEIC":
+                newName = newName + ".HEIC";
                 break;
 
-              case 'MP4':
-                newName = newName + '.MP4';
+              case "MP4":
+                newName = newName + ".MP4";
                 break;
 
-              case 'MOV':
-                newName = newName + '.MOV';
+              case "MOV":
+                newName = newName + ".MOV";
                 break;
 
-              case 'M4V':
-                newName = newName + '.M4V';
+              case "M4V":
+                newName = newName + ".M4V";
                 break;
 
               default:
                 break;
             }
-            console.log('newName >>> ', newName)
+            console.log("newName >>> ", newName);
             reName(oldName, newName);
           }
           if (isDir) {
